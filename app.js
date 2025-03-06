a5b3211089d71be3c4d77458a3809ec7a95aa42f
Guilherme Vieira <67739314+guivconB@users.noreply.github.com>
2025-03-06 14:52:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app-get('/', function(req, res){
+    res.write('Hello World! Você chegou ao top inicial!');
+    res.end('Hello World!');
+});
+
+app.listen(8080);
